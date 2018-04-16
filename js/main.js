ec3dcee4e4fcae87ec36f4d4119f9152b58f00b0
alexcodes <jordenblacksnk@gmail.com>
2018-04-16 23:01:10 +0300

    Готовит проект для верстки

--- js/main.js
+++ js/main.js
@@ -1,13 +1,13 @@
 //
-//	Copyright
+//  Copyright
 //
-//	Design: htmlacademy.ru
-//	Front end realization: Alexander Grigoriev
-//	
-//	Contact me on
-//	- fb.com/hatehtml
-//	- fuckhtml.com
-//	- github.com/fuchhtml
-//	
-//	❤ Thanks for watching my code ❤
+//  Design: htmlacademy.ru
+//  Front end realization: Alexander Grigoriev
+//  
+//  Contact me on
+//  - fb.com/hatehtml
+//  - fuckhtml.com
+//  - github.com/fuchhtml
+//  
+//  ❤ Thanks for watching my code ❤
 //

eec8ac0a7830f860080d86a6956cd793096d254c
alexcodes <jordenblacksnk@gmail.com>
2018-04-16 22:18:02 +0300

    Подготавливает файловую структуру

---
+++ js/main.js
@@ -0,0 +1,13 @@
+//
+//	Copyright
+//
+//	Design: htmlacademy.ru
+//	Front end realization: Alexander Grigoriev
+//	
+//	Contact me on
+//	- fb.com/hatehtml
+//	- fuckhtml.com
+//	- github.com/fuchhtml
+//	
+//	❤ Thanks for watching my code ❤
+//
